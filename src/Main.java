d3f83ddbe3218ad02d885587fa77f983d9b48ace
pradishan <k.pradeeshan4@gmail.com>
2022-07-04 14:41:12 +0530

    file error

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello medze!");
+        System.out.println("lets begin");
+        System.out.println("1234");
+    }
+
+}

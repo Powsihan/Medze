238626f8766a9a14c936486ffceb2777bcd7cf9a
Joe <joeravikumar99@gmail.com>
2022-08-12 13:44:19 +0530

    Comments update

--- src/pradee/PradiArray.java
+++ src/pradee/PradiArray.java
@@ -1,9 +1,9 @@
 package pradee;
-import java.io.Serializable;
+import java.io.Serializable; //Use the array as a list
 import java.util.*;
-
-public class PradiArray<T> implements Iterable<T>, Serializable {//variables - methods
-    private static final int initialCapacity = 2;
+//<T> is used to generalize data type
+public class PradiArray<T> implements Iterable<T>, Serializable { //iterable-For some iterations in the dynamic array
+    private static final int initialCapacity = 2; //Allocates a small memory for the arrays at the initial state
         private T arr[];
         private int size;
         private int capacity;
@@ -14,25 +14,17 @@ public class PradiArray<T> implements Iterable<T>, Serializable {//variables - m
             arr = (T[]) new Object[initialCapacity];
             capacity = initialCapacity;
         }
-
-        public void add(T val) {
+        private void expandArray() { //The size of array will be doubled
+            capacity *= 2;
+            arr = java.util.Arrays.copyOf(arr,capacity);
+        }
+        public void add(T val) { //Check the initial size is sufficient
             if(size==capacity)
                 expandArray();
             arr[size++] = val;
 
         }
-
-        private void expandArray() {
-            capacity *= 2;
-            arr = java.util.Arrays.copyOf(arr,capacity);
-        }
-
-        public void display() {
-            System.out.println("Elements in the list: ");
-            for(int i=0;i<size;i++)
-                System.out.print(arr[i] + " ");
-        }
-
+        //Add an object or a value to a particular place
         public void insertAtPos(int pos,T val) {
             if(size==capacity)
                 expandArray();
@@ -42,7 +34,7 @@ public class PradiArray<T> implements Iterable<T>, Serializable {//variables - m
             size++;
         }
 
-        public void deleteAtPos(int pos) {
+        public void deleteAtPos(int pos) { //Deletes an object or a value to a particular place
 
             for(int i=pos+1;i<size;i++)
                 arr[i-1] = arr[i];
@@ -53,15 +45,15 @@ public class PradiArray<T> implements Iterable<T>, Serializable {//variables - m
                 shrinkArray();
         }
 
-        private void shrinkArray() {
+        private void shrinkArray() { //Shrink the array size if there is any extra space
             capacity /= 2;
             arr =java.util.Arrays.copyOf(arr, capacity);
         }
 
         public int length() {
             return size;
-        }
-public int serarchIndex(T val){
+        } //Access the length of the array if the user needs to change
+public int serarchIndex(T val){ //If a value is given then the respective index will be returned.
             int index = -1;
             for (int i=0;i<length();i++){
                 if (arr[i] == val){
@@ -71,15 +63,16 @@ public int serarchIndex(T val){
             return index;
 
 }
+//Removes the record by finding the index of that particular value
 public void remove(T val){
             deleteAtPos(serarchIndex(val));
 }
-public void update(T oval,T nval){
-           int i = serarchIndex(oval);
-           deleteAtPos(i);
-           insertAtPos(i,nval);
+public void update(T oval,T nval){ //Updates the record
+           int i = serarchIndex(oval); //find the index of that particular value
+           deleteAtPos(i); //Deletes the old data of the index
+           insertAtPos(i,nval); //Add the new data to that index
 }
-        public Iterator<T> iterator(){
+        public Iterator<T> iterator(){ //Iteration in dynamic array
             return new Iterator<T>(){
 
                 int index = 0;

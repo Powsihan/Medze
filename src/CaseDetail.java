d56207a97955205d979aaf490dd1d7217aca5618
mohamednusnan <nusnanam12@gmail.com>
2022-08-13 12:06:13 +0530

    Report Added

--- src/CaseDetail.java
+++ src/CaseDetail.java
@@ -9,10 +9,11 @@ public class CaseDetail implements Serializable {
     private Date currentDate;
     private String data;
 
+
     public Date getCurrentDate() {
         return currentDate;
     }
-
+    // get the current  date as string input after covert in to date and set date
     public void setCurrentDate(String currentD) throws ParseException {
         SimpleDateFormat sfd = new SimpleDateFormat("dd/MM/yyyy");
         this.currentDate = sfd.parse(String.valueOf(currentD));
@@ -27,7 +28,7 @@ public class CaseDetail implements Serializable {
 
         this.data = data;
     }
-
+    //formatting to print current date and data
     @Override
     public String toString() {
         return MedzeUtil.dateViwe(currentDate) + "\n" + data;
